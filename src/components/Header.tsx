425dba11ba4132d0faad5ebf9140632d2ff80366
Pablo Draghi <83250896+Pablodr21@users.noreply.github.com>
2023-11-06 16:48:05 -0300

    msj

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='flex justify-end h-min w-screen'>
+      <h1>hola</h1>
       <ul className='flex p-2'>
         <li className='m-4'>
           <input type="text" value={'Busqueda'} />
